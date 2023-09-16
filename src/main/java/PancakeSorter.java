faa003861a4d4eae69d81e18c6d1a9c47f0e16aa
Mortada M'Rabet <mortada.mrabet@hva.nl>
2023-09-16 12:19:19 +0200

    refactor(PancakeSorter) added flip(), changes to sort()

--- src/main/java/PancakeSorter.java
+++ src/main/java/PancakeSorter.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.List;
 
 public class PancakeSorter {
@@ -10,8 +11,24 @@ public class PancakeSorter {
     public void sort(List<Pancake> toBeSortedPancakes){
         for (Pancake pancake : toBeSortedPancakes){
             int pancakeWithLargestRadius = getLargestRadius(toBeSortedPancakes);
+            int currentPancake = toBeSortedPancakes.indexOf(pancake);
+
+            if (pancakeWithLargestRadius != currentPancake - 1){
+                flip(toBeSortedPancakes, pancakeWithLargestRadius);
+                flip(toBeSortedPancakes, currentPancake - 1);
+            }
         }
     }
+    public void flip(List<Pancake> toBeSortedPancakes, int currentPos){
+        int start = 0;
+
+        while (start < currentPos){
+            Collections.swap(toBeSortedPancakes, start, currentPos);
+            start++;
+            currentPos--;
+        }
+    }
+
     public int getLargestRadius(List<Pancake> toBeSortedPancakes){
         int maxIndex = 0;
         for (Pancake pancake : toBeSortedPancakes){
